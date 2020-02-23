db9c53f41c605ebfa882a53e65f72a501a080dcd
Ryan Bigg <radarlistener@fastmail.fm>
2020-02-24 09:45:18 +1100

    Reorder refinery directory (#2)
    This PR rearranges the code in this gem slightly, moving "library code" over into `lib`, and keeping the system dependency booting code in `system`:
    
    I've found this way of arranging code to be nicer to work with in https://github.com/radar/bix. 
    
    I've tested that this PR works by using the included `test.rb`script.

--- lib/refinery.rb
+++ lib/refinery.rb
@@ -1,3 +1,21 @@
 # frozen_string_literal: true
 
-require_relative '../system/boot'
+require 'pathname'
+require 'dry/system/components'
+require 'dry/system/container'
+require 'i18n'
+
+module Refinery
+  class Container < Dry::System::Container
+    configure do |config|
+      config.root = File.expand_path('..', __dir__)
+      config.default_namespace = 'refinery'
+
+      config.auto_register = 'lib'
+    end
+
+    load_paths! 'lib'
+
+    ::I18n.load_path << Dir[config.root.join('config', 'locales', '*.yml')]
+  end
+end
